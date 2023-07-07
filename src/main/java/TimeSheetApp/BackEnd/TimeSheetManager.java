65bd8b877573b52b95d129cbf98b8764338937b3
Danilo00Freitas <danilosfreitass@gmail.com>
2023-07-06 22:02:42 -0300

    Atualização 06/07 - Atualização do construtor da classe TimeSheetManager, com a funcionalidade de verificação e criação da planilha.

--- src/main/java/TimeSheetApp/BackEnd/TimeSheetManager.java
+++ src/main/java/TimeSheetApp/BackEnd/TimeSheetManager.java
@@ -18,31 +18,4 @@ public class TimeSheetManager {
     public void addRegister(TimeSheetRecorder newRegister){
         registerList.add(newRegister);
     }
-
-    /*public boolean exportingRegisterList(String archiveName){
-        Workbook workbook = new XSSFWorkbook();
-        Sheet sheet = workbook.createSheet("Time sheet register");
-
-        int rowIndex = 0;
-        for (TimeSheetRecorder register : registerList) {
-            Row row = sheet.createRow(rowIndex++);
-            Cell dateCell = row.createCell(0);
-            dateCell.setCellValue(register.getDate());
-            Cell entryCell = row.createCell(1);
-            entryCell.setCellValue(register.getEntryTime());
-            Cell breakCell = row.createCell(2);
-            breakCell.setCellValue(register.getBreakTime());
-            Cell returnCell = row.createCell(3);
-            returnCell.setCellValue(register.getReturnTime());
-            Cell exitCell = row.createCell(4);
-            exitCell.setCellValue(register.getExitTime());
-        }
-
-        try (FileOutputStream fileOut = new FileOutputStream(archiveName)){
-            workbook.write(fileOut);
-            return true;
-        } catch (IOException e){
-            return false;
-        }
-    }*/
 }
